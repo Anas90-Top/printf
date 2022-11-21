753559e7e2e284f8bf9720cef78fb26a5ef322c2
Khalidgamar <khgamar97@gmail.com>
2022-11-21 13:46:40 -0800

    tasks

--- main.h
+++ main.h
@@ -1,23 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
  * struct structprint - structure containing
  * @q: the location and method to translate data to characters.
  * @u: print function for specific type.
- *
  * Return: int
  */
 typedef struct structprint
 {
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
-
+char *q;
+int (*u)(char *format, va_list);
+}
+structype;
 int _putchar(char ch);
 int _puts(char *string);
 int printc(char *format, va_list);
